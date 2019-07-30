5bb5aed96fdad1df55d43f265fc9180c30e12162
ysk <y.sawa4787@gmail.com>
2019-07-30 20:19:45 +0900

    カテゴリー別ページを見出しリストペーアイと同様に調整

--- resources/views/post/find_reslut.blade.php
+++ resources/views/post/find_reslut.blade.php
@@ -7,11 +7,13 @@
     {{ Breadcrumbs::render(Route::currentRouteName(), $items[0]) }}
     <div class="">
         <div class="pt-1">
+            @if(!empty($items[0]))
             <span class="h6 text-white">作成対象件数</span>
             <p id="merge_cnt" class="h1 text-white d-inline-block align-middle m-1">0</p>
             <button type="button" class="btn btn-sm btn-secondary d-inline-block align-middle" data-toggle="modal" data-target="#exampleModal">
                 記事作成
             </button>
+            @endif
         </div>
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
@@ -55,7 +57,6 @@
 @endsection
 
 @section('contents')
-
     @foreach ($items as $item)
         <div class="col-xl-3 mb-5 mb-xl-5">
             <div class="card shadow">
@@ -88,17 +89,17 @@
                         <div class="col-md-12">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="modal-title-default">{{ $item->post_title }}</h5>
+                                    <h2 class="modal-title" id="modal-title-default">{{ $item->post_title }}</h2>
                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                         <span aria-hidden="true">×</span>
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                    {!! $item->post_text !!}
+                                    {!! GitDown::parseAndCache($item->post_text) !!}
                                 </div>
                                 <div class="modal-footer">
                                     <a href="{{ route('post_edit', ['id' => $item->id]) }}" class="btn btn-primary">編集</a>
-                                    <a href="#" class="remove btn btn-danger" data-remove-id="{{ $item->id }}" data-toggle="modal" data-target="#modal-remove-post">
+                                    <a href="#" class="remove btn btn-danger" data-remove-id="{{ $item->id }}" data-remove-category-id="{{ $item->category['id'] }}" data-toggle="modal" data-target="#modal-remove-post">
                                         削除
                                     </a>
                                     <a href="" class="btn btn-link  ml-auto" data-dismiss="modal">キャンセル</a>
